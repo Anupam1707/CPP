52bbfdd5a3a7d8a7023fc5cce7556967488025d7
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-31 14:51:14 +0530

    Create cout.cpp

---
+++ Experiment 2/cout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char Name[20] = "Anupam Kanoongo";
+    int Age = 17;
+    char City[20] = "Indore";
+    char Gender[6] = "Male";
+    int Height = 170;
+    char insti[10] = "NMIMS";
+    char proff[20] = "Programmer";
+    cout << "Name : " << Name << endl;
+    cout << "Age : " << Age << endl;
+    cout << "Gender : " << Gender << endl;
+    cout << "City : " << City << endl;
+    cout << "Height : " << Height << endl;
+    cout << "Institute : " << insti << endl;
+    cout << "Profession : " << proff << endl;
+};
