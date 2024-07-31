2e6ee509e6b6ed3e690d0ab5db07687df660b6f2
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-31 14:34:42 +0530

    Create sumof2numinput.cpp

---
+++ sumof2numinput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, sum;
+    cout << "Enter the value of a : ";
+    cin >> a;
+    cout << "\nEnter the value of b : ";
+    cin >> b;
+    sum = a + b;
+    cout << "Sum of a = " << a << " and b = " << b << " is : " << sum;
+}
