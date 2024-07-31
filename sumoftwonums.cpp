fc15285e95a5ad87ce1a6b0d434c8de7ee7815f3
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-07-31 14:31:31 +0530

    Create sumoftwonums.cpp

---
+++ sumoftwonums.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    a = 23;
+    b = 45;
+    c = a + b;
+    cout << "Sum of a = " << a << " and b = " << b << " is : " << c;
+    return 0;
+}
