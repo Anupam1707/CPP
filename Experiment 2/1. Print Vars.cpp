e3c1678ebac962898508ef92cdbb2b79f5220f10
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-04 11:11:15 +0530

    Update 1. Print Vars.cpp

--- Experiment 2/1. Print Vars.cpp
+++ Experiment 2/1. Print Vars.cpp
@@ -6,13 +6,13 @@ int main() {
     char City[20] = "Indore";
     char Gender[6] = "Male";
     int Height = 170;
-    char insti[10] = "NMIMS";
-    char proff[20] = "Programmer";
+    char Institue[10] = "NMIMS";
+    char Profession[20] = "Programmer";
     cout << "Name : " << Name << endl;
     cout << "Age : " << Age << endl;
     cout << "Gender : " << Gender << endl;
     cout << "City : " << City << endl;
     cout << "Height : " << Height << endl;
-    cout << "Institute : " << insti << endl;
-    cout << "Profession : " << proff << endl;
+    cout << "Institute : " << Institute << endl;
+    cout << "Profession : " << Profession << endl;
 };
