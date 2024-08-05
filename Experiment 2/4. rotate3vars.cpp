798678a50e31f64171b9eee82c901807480d7b47
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-05 21:29:54 +0530

    Update 4. rotate3vars.cpp

--- Experiment 2/4. rotate3vars.cpp
+++ Experiment 2/4. rotate3vars.cpp
@@ -10,7 +10,7 @@ int main()
     cout << "Enter the value of Number 2 : ";
     cin >> num2;
     cout << "Enter the value of Number 3 : ";
-    cin >> num3
+    cin >> num3;
     cout << "Before Swap Number 1 = " << num1 << " Number 2 = " << num2 << " Number 3 = " << num3 << endl;
     num3 = num1 * num2 * num3;
     num2 = num3 / (num1 * num2);
