9a07ad5c531299ebc2516c60e67f95231e6b978a
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-13 21:23:15 +0530

    Update 2. secondLargeNum.cpp

--- Experiment 3/2. secondLargeNum.cpp
+++ Experiment 3/2. secondLargeNum.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int n1, n2, n3, secondLarge;
-    cout << "Enter the First Number : ";
+    
+    cout << "Enter the First Number: ";
     cin >> n1;
-    cout << "Enter the Second Number : ";
+    cout << "Enter the Second Number: ";
     cin >> n2;
-    cout << "Enter the Third Number : ";
+    cout << "Enter the Third Number: ";
     cin >> n3;
 
-    if (n1 > n2) {
-        if (n1 > n3 ) {
-            if (n2 > n3) secondLarge = n2;
-            else secondLarge = n3;
-        }
-        else {
-            if (n1 > n2) secondLarge = n1;
-            else secondLarge = n2;
-        }
+    if ((n1 > n2 && n1 < n3) || (n1 < n2 && n1 > n3)) {
+        secondLarge = n1;
+    } else if ((n2 > n1 && n2 < n3) || (n2 < n1 && n2 > n3)) {
+        secondLarge = n2;
+    } else {
+        secondLarge = n3;
     }
-    else {
-        if (n2 > n3 ) {
-            if (n1 > n3) secondLarge = n1;
-            else secondLarge = n3;
-        }
-        else {
-            if (n1 > n2) secondLarge = n1;
-            else secondLarge = n2;
-        }
-    }
-    cout << "The Second Largest Number is : " << secondLarge;
+
+    cout << "The Second Largest Number is: " << secondLarge;
 }
