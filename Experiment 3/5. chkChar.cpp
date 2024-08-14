9fbfd831c7021e245d755cd1de3cff7b255823db
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-14 14:16:11 +0530

    Create 5. chkChar.cpp

---
+++ Experiment 3/5. chkChar.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+    ch = tolower(ch);
+
+    if (ch >= 'a' && ch <= 'z') {
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                cout << "Vowel";
+                break;
+            default:
+                cout << "Consonant";
+        }
+    } else {
+        cout << "Not a letter";
+    }
+}
