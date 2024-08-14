ec4d4e905990e4776f9c16125ba95206d08f9f99
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-14 14:28:08 +0530

    Create 7. calculator.cpp

---
+++ Experiment 3/7. calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2, result;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter the First Number : ";
+    cin >> num1;
+    cout << "Enter the Second Number : ";
+    cin >> num2;
+
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 == 0) {
+                cout << "Error: Division by zero" << endl;
+                return 1;
+            }
+            result = num1 / num1;
+            break;
+        default:
+            cout << "Invalid operator" << endl;
+            return 1;
+    }
+    cout << "Result: " << result << endl;
+}
