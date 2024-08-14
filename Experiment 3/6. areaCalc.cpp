c3abb5a660ed6800d7e954666f5d062b5d97918a
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-14 14:24:19 +0530

    Create 6. areaCalc.cpp

---
+++ Experiment 3/6. areaCalc.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+    const float pi = 3.14;
+    double base, height, length, width, radius, area;
+
+    cout << "Welcome to the Area Calculator!\n";
+    cout << "1. Area of a Triangle\n";
+    cout << "2. Area of a Rectangle\n";
+    cout << "3. Area of a Circle\n";
+    cout << "4. Area of a Sphere\n";
+    cout << "Enter your Choice : ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            cout << "Enter base and height of the triangle: ";
+            cin >> base >> height;
+            area = 0.5 * base * height;
+            cout << "Area of the triangle: " << area << endl;
+            break;
+        case 2:
+            cout << "Enter length and width of the rectangle: ";
+            cin >> length >> width;
+            area = length * width;
+            cout << "Area of the rectangle: " << area << endl;
+            break;
+        case 3:
+            cout << "Enter radius of the circle: ";
+            cin >> radius;
+            area = pi * radius * radius;
+            cout << "Area of the circle: " << area << endl;
+            break;
+        case 4:
+            cout << "Enter radius of the sphere: ";
+            cin >> radius;
+            area = 4 * pi * radius * radius;
+            cout << "Area of the sphere: " << area << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+    }
+}
