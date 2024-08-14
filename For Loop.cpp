0e41de5c18ba179b60211fb10dc3fe0d0d14cfe2
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-14 15:26:59 +0530

    Create For Loop.cpp

---
+++ For Loop.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        cout << i << endl;
+    }
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << i << endl;
+    }
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int sm = 0;
+    cout << "Enter a number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        sm += i;
+    }
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int fact = 1;
+    cout << "Enter a number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        sm *= i;
+    }
+}
