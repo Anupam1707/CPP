df125c1ec35f4858897f8460aede272276545622
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-17 11:19:26 +0530

    Create salesDiscountCalculator.cpp

---
+++ Experiment 3/Additional/salesDiscountCalculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double salesAmount, discount, amountAfterDiscount;
+    cout << "Enter the sales amount : ";
+    cin >> salesAmount;
+
+    if (salesAmount > 5000) {
+        discount = salesAmount * 0.12;
+    } else {
+        discount = salesAmount * 0.07;
+    }
+
+    amountAfterDiscount = salesAmount - discount;
+
+    cout << "Total discount: $" << discount << endl;
+    cout << "Amount to be paid after discount: $" << amountAfterDiscount << endl;
+}
