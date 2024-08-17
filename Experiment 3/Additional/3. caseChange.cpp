9191cda1cb637532c2c68e6114c7100fd1e00416
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-17 11:26:26 +0530

    Update 3. caseChange.cpp

--- Experiment 3/Additional/3. caseChange.cpp
+++ Experiment 3/Additional/3. caseChange.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
-
 int main() {
     char ch;
-    cout << "Enter a character : ";
+    cout << "Enter a character: ";
     cin >> ch;
-    if (ch >= 'A' && ch <= 'Z') {
+    if (isupper(ch)) {
         cout << "The character is a capital letter." << endl;
-        cout << "Converted to small letter : " << (char)(ch + 32) << endl;
-    } else if (ch >= 'a' && ch <= 'z') {
+        cout << "Converted to small letter: " << (char)tolower(ch) << endl;
+    } else if (islower(ch)) {
         cout << "The character is a small letter." << endl;
-        cout << "Converted to capital letter : " << (char)(ch - 32) << endl;
+        cout << "Converted to capital letter: " << (char)toupper(ch) << endl;
     } else {
         cout << "The character is neither a small nor a capital letter." << endl;
     }
