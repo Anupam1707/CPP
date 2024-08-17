0558e05b22fa9854d3c5a40fa352dee7984f6f1d
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-17 12:43:30 +0530

    Create 7. chkPrime.cpp

---
+++ Experiment 4/7. chkPrime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    bool isPrime = true;
+
+    if (num <= 1) {
+        isPrime = false;
+    } else {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+            }
+        }
+    }
+    if (isPrime) {
+        cout << num << " is a Prime Number.";
+    } else {
+        cout << num << " is not a Prime Number.";
+    }
+}
