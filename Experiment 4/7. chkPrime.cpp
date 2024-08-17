51f5440c315c8ea6c5ae3cf9020b10aaf0626f7c
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-17 12:55:55 +0530

    Update 7. chkPrime.cpp

--- Experiment 4/7. chkPrime.cpp
+++ Experiment 4/7. chkPrime.cpp
@@ -12,6 +12,7 @@ int main() {
         for (int i = 2; i <= num / 2; i++) {
             if (num % i == 0) {
                 isPrime = false;
+                break;
             }
         }
     }
