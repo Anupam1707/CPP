6414bda810ae1297274d25677d93b901faba44c4
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-08-23 19:44:09 +0530

    Update 9. gcd_lcm_maxmin.cpp

--- Experiment 4/9. gcd_lcm_maxmin.cpp
+++ Experiment 4/9. gcd_lcm_maxmin.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
     int num1, num2, temp1, temp2, gcd, lcm;
-
     cout << "Enter First Number : ";
     cin >> num1;
     cout << "Enter Second Number : ";
     cin >> num2;
-
-    temp1 = num1;
+    temp1 = num1
     temp2 = num2;
-
     while (temp2 != 0) {
         gcd = temp2;
         temp2 = temp1 % temp2;
         temp1 = gcd;
     }
     lcm = (num1 * num2) / gcd;
-
     cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd << endl;
     cout << "LCM of " << num1 << " and " << num2 << " is: " << lcm << endl;
 }
