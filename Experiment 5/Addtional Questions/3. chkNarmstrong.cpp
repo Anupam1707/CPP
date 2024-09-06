d39e1ed522170abd53b3c69d9e8269164ab3b245
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-06 08:02:07 +0530

    Update 3. chkNarmstrong.cpp

--- Experiment 5/Addtional Questions/3. chkNarmstrong.cpp
+++ Experiment 5/Addtional Questions/3. chkNarmstrong.cpp
@@ -2,24 +2,30 @@
 #include <cmath>
 using namespace std;
 int main() {
-    int num, digit, temp, count = 0, sum = 0;
-    cout << "Enter a Number to be checked : ";
-    cin >> num;
-    temp = num;
-    while (temp != 0) {
-        temp /= 10;
-        count++;
-    }
-    temp = num;
-    do {
-        digit = temp % 10;
-        sum += pow(digit, count);
-        temp /= 10;
-    } while (temp != 0);
-
-    if (sum == num) {
-        cout << num << " is an Armstrong Number";
-    } else {
-        cout << num << " is not an Armstrong Number";
+    int n1, n2, digit, temp, count = 0, sum = 0;
+    cout << "Enter the Starting Number : ";
+    cin >> n1;
+    cout << "Enter the Ending Number : ";
+    cin >> n2;
+    for (int i = n1; i <= n2; i++) {
+        sum = 0;
+        count = 0;
+        temp = n1;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+        temp = num;
+        do {
+            digit = temp % 10;
+            sum += pow(digit, count);
+            temp /= 10;
+        } while (temp != 0);
+    
+        if (sum == num) {
+            cout << num << " is an Armstrong Number";
+        } else {
+            cout << num << " is not an Armstrong Number";
+        }
     }
 }
