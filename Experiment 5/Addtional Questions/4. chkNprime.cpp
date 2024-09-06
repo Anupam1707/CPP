019c923dc492c7d2ba57165ba4ff42be195c6b2e
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-06 08:09:06 +0530

    Update 4. chkNprime.cpp

--- Experiment 5/Addtional Questions/4. chkNprime.cpp
+++ Experiment 5/Addtional Questions/4. chkNprime.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    bool isPrime = true;
-    if (num <= 1) {
-        isPrime = false;
-    } else {
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
+    int n1, n2, digit, temp, count = 0, sum = 0;
+    cout << "Enter the Starting Number : ";
+    cin >> n1;
+    cout << "Enter the Ending Number : ";
+    cin >> n2;
+    for (int i = n1; i <= n2; i++) {
+        count = 0;
+        sum = 0;
+        bool isPrime = true;
+        if (i <= 1) {
+            isPrime = false;
+        } else {
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
         }
-    }
-    if (isPrime) {
-        cout << num << " is a Prime Number.";
-    } else {
-        cout << num << " is not a Prime Number.";
+        if (isPrime) {
+            cout << i << " is a Prime Number" << endl;
+        }
     }
 }
