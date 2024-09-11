0288974462f1ac39f63f3c6b2a4e3a126bcb865a
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-11 14:18:54 +0530

    Create 5. exchange1and3.cpp

---
+++ Experiment 6/5. exchange1and3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[4] = {1,2,3,4};
+    int temp;
+    cout <<"Original Array : ";
+    for (int i = 0; i <4; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    temp = arr[0];
+    arr[0] = arr[3];
+    arr[3] = temp;
+    cout << "Modified Array : ";
+    for (int i = 0; i < 4; i++) {
+        cout << arr[i] << " ";
+    }
+}
