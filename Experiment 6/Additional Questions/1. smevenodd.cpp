78dbdbbf9c6ff6a8060dc31ba6beea270ac15a36
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 07:33:24 +0530

    Create 1. smevenodd.cpp

---
+++ Experiment 6/Additional Questions/1. smevenodd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sumOdd = 0;
+    int sumEven = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            sumEven += arr[i];
+        } else {
+            sumOdd += arr[i];
+        }
+    }
+
+    cout << "Sum of odd numbers: " << sumOdd << endl;
+    cout << "Sum of even numbers: " << sumEven << endl;
+}
