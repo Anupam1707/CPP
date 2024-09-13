382e7b410b8171bd95ad6f68b2919b9ed9ab8c76
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 13:44:55 +0530

    Update 9. matrixmulti.cpp

--- Experiment 6/9. matrixmulti.cpp
+++ Experiment 6/9. matrixmulti.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 int main() {
     int m, n, p, q;
-
     cout << "Enter dimensions for first matrix (m n): ";
     cin >> m >> n;
     int A[m][n];
-
     cout << "Enter elements of first matrix:" << endl;
     for (int i = 0; i < m; ++i)
         for (int j = 0; j < n; ++j)
             cin >> A[i][j];
-
     cout << "Enter dimensions for second matrix (p q): ";
     cin >> p >> q;
     int B[p][q];
-
     cout << "Enter elements of second matrix:" << endl;
     for (int i = 0; i < p; ++i)
         for (int j = 0; j < q; ++j)
             cin >> B[i][j];
-
     if (n != p) {
         cout << "Matrix multiplication not possible." << endl;
         return 1;
     }
-
     int C[m][q];
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < q; ++j) {
@@ -34,7 +28,6 @@ int main() {
                 C[i][j] += A[i][k] * B[k][j];
         }
     }
-
     cout << "Resultant matrix:" << endl;
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < q; ++j)
