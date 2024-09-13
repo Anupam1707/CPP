d3b94afe971b05036ec7825bfec70a4f4c28519b
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 23:29:04 +0530

    Update 2. dispevenodd.cpp

--- Experiment 6/Additional Questions/2. dispevenodd.cpp
+++ Experiment 6/Additional Questions/2. dispevenodd.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
     int n;
-
     cout << "Enter the size of the array: ";
     cin >> n;
-
     int arr[n];
-
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     cout << "Odd numbers: ";
     for (int i = 0; i < n; i++) {
         if (arr[i] % 2 != 0) {
@@ -22,7 +16,6 @@ int main() {
         }
     }
     cout << endl;
-
     cout << "Even numbers: ";
     for (int i = 0; i < n; i++) {
         if (arr[i] % 2 == 0) {
