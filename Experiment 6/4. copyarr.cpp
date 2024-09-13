1b69e12d9af625a51704884345b795fe632d4ea7
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 08:42:28 +0530

    Update 4. copyarr.cpp

--- Experiment 6/4. copyarr.cpp
+++ Experiment 6/4. copyarr.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int arr1[5]; arr2[5];
+    int arr1[5] = {1, 2, 3, 4, 5}, arr2[5];
+    
     for (int i = 0; i < 5; i++) {
-        cin >> arr1[i];
+        arr2[i] = arr1[4 - i];
     }
     for (int i = 0; i < 5; i++) {
-        arr2[i] = arr1[i];
+        cout << arr2[i] << " ";
     }
 }
