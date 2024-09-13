afb7de1be1a0442aa11f192587bf847827c3d674
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 07:44:07 +0530

    Create 6. copyarranddisp.cpp

---
+++ Experiment 6/Additional Questions/6. copyarranddisp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr1[n];
+    int arr2[n];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    // Copy elements from arr1 to arr2
+    for (int i = 0; i < n; i++) {
+        arr2[i] = arr1[i];
+    }
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Copied array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+}
