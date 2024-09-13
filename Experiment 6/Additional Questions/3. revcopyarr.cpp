2a18ac33a54e48bb97b3b7001dc26f2b370c91e8
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 07:36:56 +0530

    Create 3. revcopyarr.cpp

---
+++ Experiment 6/Additional Questions/3. revcopyarr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr1[n];
+    int arr2[n];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr2[i] = arr1[n - i - 1];
+    }
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+}
