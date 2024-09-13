449cde279c1e2be117ed7ace4a7ab5bc497e8d26
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 07:50:02 +0530

    Create 8. sumuppertriangle.cpp

---
+++ Experiment 6/Additional Questions/8. sumuppertriangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m, n;
+
+    cout << "Enter the number of rows: ";
+    cin >> m;
+    cout << "Enter the number of columns: ";
+    cin >> n;
+
+    int matrix[m][n];
+
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int sumUpperTriangular = 0;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i; j < n; j++) {
+            sumUpperTriangular += matrix[i][j];
+        }
+    }
+
+    cout << "Sum of upper triangular elements: " << sumUpperTriangular << endl;
+}
