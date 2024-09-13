5004ce9ff7fb69e120d291d2291ca3bff043b1f0
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-13 08:56:23 +0530

    Update 7. sumoflowertriangle.cpp

--- Experiment 6/7. sumoflowertriangle.cpp
+++ Experiment 6/7. sumoflowertriangle.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int matrix[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    int sum = 0;
-
+    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, sum = 0;
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < (i < 3 ? i : 3); ++j) {
             sum += matrix[i][j];
