c1bdc54635da33030681ac78280a6fdb210f1993
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-14 08:51:16 +0530

    Update 7. sumdiagonal.cpp

--- Experiment 6/Additional Questions/7. sumdiagonal.cpp
+++ Experiment 6/Additional Questions/7. sumdiagonal.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
     int m, n;
-
     cout << "Enter the number of rows: ";
     cin >> m;
     cout << "Enter the number of columns: ";
     cin >> n;
-
     int matrix[m][n];
-
     cout << "Enter the elements of the matrix:\n";
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             cin >> matrix[i][j];
         }
     }
-
     int sumDiagonal1 = 0;
-
     for (int i = 0; i < m; i++) {
         sumDiagonal1 += matrix[i][i];
     }
