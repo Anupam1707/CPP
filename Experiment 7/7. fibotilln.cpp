3d68a91effd0ccd8ea058441c0c453ba3768872c
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-09-25 15:49:02 +0530

    Update 7. fibotilln.cpp

--- Experiment 7/7. fibotilln.cpp
+++ Experiment 7/7. fibotilln.cpp
@@ -9,14 +9,12 @@ int fibonacci(int n) {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
-
 void print_fibonacci_series(int n) {
     for (int i = 1; i <= n; ++i) {
         cout << fibonacci(i) << " ";
     }
     cout << endl;
 }
-
 int main() {
     int n;
     cout << "Enter the number of terms in the Fibonacci series: ";
