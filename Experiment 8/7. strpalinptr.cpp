7ab3d523fe8c2132d16e8b355a1629f5bac748bb
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2024-10-06 01:14:29 +0530

    Update 7. strpalinptr.cpp

--- Experiment 8/7. strpalinptr.cpp
+++ Experiment 8/7. strpalinptr.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
-
 bool isPalindrome(const char* str, int size) {
     const char* left = str;
     const char* right = str + size - 1;
-
     while (left < right) {
         if (*left != *right)
             return false;
@@ -13,7 +11,6 @@ bool isPalindrome(const char* str, int size) {
     }
     return true;
 }
-
 int main() {
     char str[100];
     cout << "Enter a string: ";
